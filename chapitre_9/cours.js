4b66c4bdb6ce25cc7363bba380a35fe68277e802
pookie86 <lalaina36@gmail.com>
2020-04-25 13:50:45 +0200

    :memo: add chapitre_9

--- chapitre_9/cours.js
+++ chapitre_9/cours.js
@@ -1,38 +1,101 @@
 
 
-var news = document.getElementById("articles");
+
+var contenu = document.getElementById("articles");
 
 ajaxGet("https://oc-jswebsrv.herokuapp.com/api/articles", function (reponse) {
 
     var articles = JSON.parse(reponse);
 
-        articles.forEach(article => {
+        articles.forEach(function (article) {
 
             var titre = document.createElement("h2");
                 titre.textContent = article.titre;
 
-            var contenu = document.createElement("p");
-                contenu.textContent =article.contenu;
+            var texte = document.createElement("p");
+                texte.textContent = article.contenu;
 
-        news.appenChild(titre);
-        news.appenChild(contenu);
-        });
+    contenu.appendChild(titre);
+    contenu.appendChild(texte);
 
+    });
 });
 
 
-var premMinElt = document.getElementById("premMin");
+var premierMinistre = document.getElementById("premMin");
 
 ajaxGet("https://www.data.gouv.fr/api/1/organizations/premier-ministre/", function (reponse) {
 
-    var premierMinistre = JSON.parse(reponse);
+    var premierMin = JSON.parse(reponse);
 
-    var descriptionElt = document.createElement("p");
-        descriptionElt.textContent = premierMinistre.description;
+    var description = document.createElement("p");
+        description.textContent = premierMin.description;
 
-    var logoElt = document.createElement("img");
-        logoElt.src = premierMinistre.logo;
+    var logo = document.createElement("img");
+        logo.src = premierMin.logo;
 
-    premMinElt.appendChild(descriptionElt);
-    premMinElt.appendChild(logoElt);
+    premierMinistre.appendChild(description);
+    premierMinistre.appendChild(logo);
 });
+
+
+// choper la div pour afficher les infos
+
+    var infos = document.getElementById("infos");
+
+// choper le form event Submit
+    var form = document.querySelector("form");
+
+        form.addEventListener("submit", function (e) {
+            // supprimer e par defaut
+            e.preventDefault();
+
+            // choper le nom profil dans la valeur de l'element du form
+            var pseudo = form.elements.profil.value; //name
+
+            ajaxGet("https://api.github.com/users/" + pseudo, function (reponse) {
+
+                // parse la reponse
+                var profil = JSON.parse(reponse);
+
+                // creer et choper img, name, company, blog, href
+                var card = document.getElementById("card");
+
+                var image = document.createElement("img");
+                    image.src = profil.avatar_url;
+                    image.id = "avatar";
+
+                var nom = document.createElement("p");
+                    nom.textContent = profil.name;
+
+                var lien = document.createElement("a");
+                    lien.textContent = profil.html_url;
+                    lien.href = profil.html_url;
+
+                var repo = document.createElement("p");
+                    repo.textContent = profil.public_repos;
+
+                var bio = document.createElement("p");
+                    bio.textContent = profil.bio;
+
+                var lieu = document.createElement("p");
+                    lieu.textContent = profil.location;
+
+                var information = image.outerHTML + nom.outerHTML + lien.outerHTML + repo.outerHTML + bio.outerHTML + lieu.outerHTML;
+
+                    card.innerHTML += "<div class='card'>" + information +"</div>";
+
+            });
+
+        });
+
+
+
+
+
+
+
+
+
+
+
